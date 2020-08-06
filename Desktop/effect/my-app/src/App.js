e58f7e52a56ed6afd4360a65ecd78713fe222b8e
Emmanuel Iyanu <emmanueliyanu2012@gmail.com>
2020-08-06 17:02:32 +0100

    few changes

--- Desktop/effect/my-app/src/App.js
+++ Desktop/effect/my-app/src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+
+  state = {
+    countries: []
+  }
+
+  addCountry() {
+    this.setState({ countries: [...this.state.countries, ""] });
+  }
+
+  handleChange(e, index) {
+    this.state.countries[index] = e.target.value
+
+    //set the changed state
+    this.setState({ countries: this.state.countries })
+  }
+
+  handleRemove(index) {
+    //remove the index field
+    this.state.countries.splice(index, 1)
+    console.log(this.state.countries, "sss");
+
+    //update the state
+    this.setState({ countries: this.state.countries })
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>The form</h2>
+        <label>Address</label>
+        {
+          this.state.countries.map((country, index) => {
+            return (
+              <div key={index}>
+                <input onChange={(e) => this.handleChange(e, index)} value={country} />
+
+                <button onClick={() => this.handleRemove(index)}>Remove</button>
+              </div>
+            )
+          })
+        }
+        <hr />
+        <button onClick={(e) => this.addCountry(e)}>Add Country</button>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
