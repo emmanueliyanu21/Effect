ef76401635eff9a70c77f696c7143ac2e457a195
Emmanuel Iyanu <emmanueliyanu2012@gmail.com>
2020-08-07 18:24:11 +0100

    few changes

---
+++ Desktop/effect/task-form/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import TaskForm from './component/taskForm.js'
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="App">
+      <TaskForm />
+    </div>
+  );
+}
+
+export default App;
